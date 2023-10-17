a7587313308906d02e4006e302673a7fd0456502
Justin Jeong <justin.jeong@buzzvil.com>
2023-10-17 14:14:12 +0900

    add: lazy loading for sub app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
@@ -6,8 +6,9 @@ import {
 } from "@material-ui/core/styles";
 
 import Header from "./components/Header";
-import AuthApp from "./components/AuthApp";
-import MarketingApp from "./components/MarketingApp";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -19,10 +20,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <Switch>
-            <Route path="/auth" component={AuthApp} />
-            <Route path="/" component={MarketingApp} />
-          </Switch>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
